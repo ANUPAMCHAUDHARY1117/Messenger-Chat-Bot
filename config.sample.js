0e1d5e23e4b85bf224ba0543cce5ac60f65dc431
Anupam Chaudhary <anupam1996@gmail.com>
2018-10-08 23:23:20 +0530

    Facebook Messenger

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAACfNf5kpIgBAGGrgsy1Oje25ZBlJpVfggpnRlB1stxhsJyASx7foDIWLV18Wm8y43OLGxV9ChZBQbwr4kod6mtgyIgZBSqFF45DALeZBss1hheAwh0DAvCiWxNV4MoPRXvM6rjZB2OYeSlZBZBLGAgc1UxOZAtxAfAZAp9gosnu2IQZDZD',
+    FB_VERIFY_TOKEN: 'this_is_a_smart_chat_bot',
+    FB_APP_SECRET: 'bb55fa3f19c34c26ceeaf62a23f98170',
+    SERVER_URL: 'https://smartbot-messenger.herokuapp.com/',
+    GOOGLE_PROJECT_ID: 'smartchatbot-d3dcc',
+    DF_LANGUAGE_CODE: 'en',
+    GOOGLE_CLIENT_EMAIL: 'anupam1996@gmail.com',
+    GOOGLE_PRIVATE_KEY: 'ecd220bce3c5c5c1206a3105f40ff6d48dcd30a3'
+};
